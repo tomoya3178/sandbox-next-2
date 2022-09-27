0e9535a5a8cd7e7b952bdc9cc85ba5d1975a7e12
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2022-09-27 10:21:52 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,40 @@
-import { Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
+import { Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
 import MailIcon from '@mui/icons-material/Mail';
 import { NextPage } from "next"
 
 const Home: NextPage = () => {
   return (
-    <Drawer
-      variant="permanent"
-    >
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon><MailIcon /></ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </ Drawer>
+    <Stack>
+      <Drawer
+        variant="permanent"
+      >
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon><MailIcon /></ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </ Drawer>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert</TableCell>
+              <TableCell>Calories</TableCell>
+              <TableCell>Fat</TableCell>
+              <TableCell>Carbs</TableCell>
+              <TableCell>Protein</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          </TableBody>
+        </Table>
+      </ TableContainer>
+    </Stack>
   )
 }
 
